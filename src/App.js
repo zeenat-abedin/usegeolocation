fbae92ded2e4be985320783a7b963cc58de94f6d
zeenat-abedin <zeenu.23@gmail.com>
2024-01-10 20:41:22 +0530

    Add geolocation feature to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,59 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useState } from "react";
 
-export default App;
+function useGeolocation() {}
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countClicks, setCountClicks] = useState(0);
+  const [position, setPosition] = useState({});
+  const [error, setError] = useState(null);
+
+  const { lat, lng } = position;
+
+  function getPosition() {
+    setCountClicks((count) => count + 1);
+
+    if (!navigator.geolocation)
+      return setError("Your browser does not support geolocation");
+
+    setIsLoading(true);
+    navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        setPosition({
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude,
+        });
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error.message);
+        setIsLoading(false);
+      }
+    );
+  }
+
+  return (
+    <div>
+      <button onClick={getPosition} disabled={isLoading}>
+        Get my position
+      </button>
+
+      {isLoading && <p>Loading position...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && lat && lng && (
+        <p>
+          Your GPS position:{" "}
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={`https://www.openstreetmap.org/#map=16/${lat}/${lng}`}
+          >
+            {lat}, {lng}
+          </a>
+        </p>
+      )}
+
+      <p>You requested position {countClicks} times</p>
+    </div>
+  );
+}
